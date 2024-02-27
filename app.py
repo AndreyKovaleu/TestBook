3ee3c62d00a6e0f824a81f4c49bacbdd6f6f7b43
AndreyKovaleu <Andreykovaleu@gmail.com>
2024-02-28 00:22:34 +0300

    аfirst commit

---
+++ app.py
@@ -0,0 +1,32 @@
+import asyncio
+import logging
+
+from aiogram import Bot, Dispatcher
+from aiogram.enums import ParseMode
+
+from config_data.config import Config, load_config
+from keyboards.main_menu import set_main_menu
+
+
+# from handlers import router as main_handlers_router
+
+
+async def main() -> None:
+    config: Config = load_config(".env")
+
+    bot = Bot(token=config.tg_bot.token, parse_mode=ParseMode.HTML)
+    dp = Dispatcher()
+
+    await set_main_menu(bot)
+
+    # dp.workflow_data.update({"admin_id": config.tg_bot.admin_ids[0]})
+    # dp.include_router(main_handlers_router)
+
+    await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO, format="%(filename)s:%(lineno)d #%(levelname)-8s "
+                                                   "[%(asctime)s] - %(name)s - %(message)s")
+    asyncio.run(main())
